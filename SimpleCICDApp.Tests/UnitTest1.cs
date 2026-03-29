29a8ec894c0ef20ca097bb3160628bffa4a1ae0c
diljankhan <diljan_n@hotmail.com>
2026-03-29 22:17:16 +0300

    Test passed

--- SimpleCICDApp.Tests/UnitTest1.cs
+++ SimpleCICDApp.Tests/UnitTest1.cs
@@ -19,7 +19,7 @@ namespace SimpleCICDApp.Tests
             int result = a + b;
 
             // Assert
-            Assert.Equal(6, result);
+            Assert.Equal(5, result);
         }
     }
 }
