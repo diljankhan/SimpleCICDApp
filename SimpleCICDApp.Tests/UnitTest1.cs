60a72277d4c818b8d0fd0397bddd7d72e654d7ea
diljankhan <diljan_n@hotmail.com>
2026-03-29 15:19:04 +0300

    Add project files.

---
+++ SimpleCICDApp.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+namespace SimpleCICDApp.Tests
+{
+    public class UnitTest1
+    {
+        //[Fact]
+        //public void Test1()
+        //{
+
+        //}
+
+        [Fact]
+        public void Test1_ShouldPass()
+        {
+            // Arrange
+            int a = 2;
+            int b = 3;
+
+            // Act
+            int result = a + b;
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+    }
+}
